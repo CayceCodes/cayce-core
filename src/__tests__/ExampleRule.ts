2a66fea50d913600210213ef5530bd95a8e67660
Justin Stroud <157166970+justinstroudbah@users.noreply.github.com>
2025-03-10 15:03:53 -0700

    Look to the validate method in the ExampleRule

--- src/__tests__/ExampleRule.ts
+++ src/__tests__/ExampleRule.ts
@@ -1,4 +1,6 @@
 import { ruleSeverity, category, context, message, name, treeQuery, ScanRule, suggestion } from 'cayce-types';
+import TsSfApex from 'tree-sitter-sfapex';
+import Parser, { Query, QueryCapture } from 'tree-sitter';
 
 
 @name('Variable, constant or parameter name too short')
@@ -7,6 +9,19 @@ import { ruleSeverity, category, context, message, name, treeQuery, ScanRule, su
 @message('This query returns all variable/parameter/constant names that are under 3 characters in length')
 @suggestion('Increase the length of the identifier')
 @ruleSeverity(3)
-@treeQuery('(variable_declarator name: (identifier)@var (#match? @var "^.{0,2}$"))' +
-    '(formal_parameter name: (identifier)@param (#match? @param "^.{0,2}$"))')
-export class ExampleRule extends ScanRule {}
+@treeQuery('((variable_declarator name: (identifier)@target)@var)')
+export class ExampleRule extends ScanRule {
+    TreeSitterLanguage = TsSfApex.apex;
+
+    validate(targetSource: string, parser: Parser): Parser.SyntaxNode[] {
+        parser.setLanguage(TsSfApex.apex);
+        const rootTree: Parser.Tree = parser.parse(targetSource);
+        const queryInstance: Query = new Query(this.TreeSitterLanguage, this.TreeQuery);
+        const results: Parser.SyntaxNode[] = [];
+        const captures: QueryCapture[] = queryInstance.captures(rootTree.rootNode);
+        captures.forEach((capture) => {
+            results.push(capture.node);
+        });
+        return results;
+    }
+}
